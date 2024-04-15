c765df66520aebed54f607c6d10e80fe6f92ade1
Anya <xxxuko@gmail.com>
2024-04-15 14:55:00 +0300

    update crud

--- crud.py
+++ crud.py
@@ -1,9 +1,10 @@
 from sqlalchemy.orm import Session
-from . import models, schemas
+import models
+import schemas
 
 
-def get_all_authors(db: Session):
-    return db.query(models.Author).all()
+def get_all_authors(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(models.Author).offset(skip).limit(limit).all()
 
 
 def get_author_by_id(db: Session, author_id: int):
@@ -21,28 +22,16 @@ def create_author(db: Session, author: schemas.AuthorCreate):
     return db_author
 
 
-def update_author(db: Session, author_id: int, author: schemas.AuthorCreate):
-    db_author = get_author_by_id(db, author_id)
-    db_author.name = author.name
-    db_author.bio = author.bio
-    db.commit()
-    db.refresh(db_author)
-    return db_author
+def get_all_books(
+        db: Session,
+        skip: int = 0,
+        limit: int = 10
+):
+    return db.query(models.Book).offset(skip).limit(limit).all()
 
 
-def delete_author(db: Session, author_id: int):
-    db_author = get_author_by_id(db, author_id)
-    db.delete(db_author)
-    db.commit()
-    return db_author
-
-
-def get_all_books(db: Session):
-    return db.query(models.Book).all()
-
-
-def get_book_by_id(db: Session, book_id: int):
-    return db.query(models.Book).filter(models.Book.id == book_id).first()
+def get_books_by_author(db: Session, author_id: int):
+    return db.query(models.Book).filter(models.Book.author_id == author_id).all()
 
 
 def create_book(db: Session, book: schemas.BookCreate):
@@ -55,22 +44,4 @@ def create_book(db: Session, book: schemas.BookCreate):
     db.add(db_book)
     db.commit()
     db.refresh(db_book)
-    return db_book
-
-
-def update_book(db: Session, book_id: int, book: schemas.BookCreate):
-    db_book = get_book_by_id(db, book_id)
-    db_book.title = book.title
-    db_book.summary = book.summary
-    db_book.publication_date = book.publication_date
-    db_book.author_id = book.author_id
-    db.commit()
-    db.refresh(db_book)
-    return db_book
-
-
-def delete_book(db: Session, book_id: int):
-    db_book = get_book_by_id(db, book_id)
-    db.delete(db_book)
-    db.commit()
     return db_book
