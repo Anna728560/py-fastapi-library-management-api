b592d2c11c22ba64ded69cec1cdf913c861c70b1
Anya <xxxuko@gmail.com>
2024-04-15 12:54:38 +0300

    Create Pydantic models for data validation and serialization in the schemas.py file

---
+++ schemas.py
@@ -0,0 +1,38 @@
+from datetime import date
+
+from pydantic import BaseModel
+
+
+class AuthorBase(BaseModel):
+    name: str
+    bio: str
+
+
+class AuthorCreate(AuthorBase):
+    ...
+
+
+class Author(AuthorBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class BookBase(BaseModel):
+    title: str
+    summary: str
+    publication_date: date
+    author_id: int
+
+
+class BookCreate(BookBase):
+    ...
+
+
+class Book(BookBase):
+    id: int
+    author: Author
+
+    class Config:
+        orm_mode = True
